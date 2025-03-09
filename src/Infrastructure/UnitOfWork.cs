4529f89b644f1d824729a6044fa3f840ef150156
milewsk <milewski.konrad00@gmail.com>
2025-03-09 21:11:53 +0100

    Added UnitOfWork.cs and ApplicationDbContext.cs

---
+++ src/Infrastructure/UnitOfWork.cs
@@ -0,0 +1,18 @@
+using Domain.Abstractions;
+
+namespace Infrastructure;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly ApplicationDbContext _context;
+
+    public UnitOfWork(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<int> SaveChangesAsync()
+    {
+        return await _context.SaveChangesAsync();
+    }
+}
